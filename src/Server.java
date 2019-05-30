350334ad37d187c5e1dd76bdc2dc12acc31b0ee4
pippo <giuseppelachina@hotmail.it>
2019-05-30 10:00:10 +0200

    addid RMIproject2

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,8 @@
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class Server extends UnicastRemoteObject implements ServerInterface {
@@ -11,13 +13,18 @@ public class Server extends UnicastRemoteObject implements ServerInterface {
     }
 
     public Server() throws RemoteException{
-
+        // modifiche se non siamo in localhost
+        // super(xxxx);
     }
 
     public static void main(String args[]){
 
         try {
+            //System.setProperties("java.rmi.server.hostname","whitelodge.ns=.it");
+            //Registry registry = LocateRegistry.getRegistry();
+
             ServerInterface server = new Server();
+            //registry.bind("Calculator", server);
             Naming.rebind("Calculator",server);
         } catch (RemoteException | MalformedURLException e) {
             e.printStackTrace();
